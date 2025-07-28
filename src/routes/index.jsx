73f688567d23dd6a7eccb6ab97ad78cc752245ec
hossam-ibrahim27 <enghossamibrahimhassan@gmail.com>
2025-07-28 15:25:06 +0300

    Team_2

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter, createRoutesFromElements, Route, Routes, } from "react-router";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            //? Here Add Your Route.
+            {/* <Route path="/" element={<HomePage />} /> */}
+        </>
+    )
+);
+export default router;
