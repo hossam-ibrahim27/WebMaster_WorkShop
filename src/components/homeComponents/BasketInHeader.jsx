e5234f1d51ae3303a4ec8774ad05a43399b50cfa
sihs-pashaa <hsynaldswqy54@gmail.com>
2025-08-03 15:21:23 +0300

    Update BasketInHeader.jsx

--- src/components/homeComponents/BasketInHeader.jsx
+++ src/components/homeComponents/BasketInHeader.jsx
@@ -1,25 +1,32 @@
-{/*//?FontAWESOME*/ }
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faBagShopping } from '@fortawesome/free-solid-svg-icons';
-import "../../../src/css/homeComponents/Header.css"
+import { faUser, faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import "../../../src/css/homeComponents/Header.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"; 
+
 const BasketInHeader = () => {
-    const [totalPriceInBasket] = useState(0);
-    const [countProductInBasket] = useState(0);
-    return (
-        <div className="padge">
-            <div className="user">
-                <FontAwesomeIcon icon={faUser} className='fonticon' />
-            </div>
-            <div className="basket">
-                <span>${totalPriceInBasket}</span>
-                <div>
-                    <FontAwesomeIcon icon={faBagShopping} className='fonticon' />
-                    <span>{countProductInBasket}</span>
-                </div>
-            </div>
+  const [totalPriceInBasket] = useState(0);
+  const [countProductInBasket] = useState(0);
+  const navigate = useNavigate(); 
+
+  return (
+    <div className="padge">
+      <div className="user cursor-pointer" onClick={() => navigate("/signup")}> 
+        <FontAwesomeIcon icon={faUser} className="fonticon" />
+      </div>
+
+      <div className="basket">
+        <span>${totalPriceInBasket}</span>
+        <div
+          className="cursor-pointer"
+          onClick={() => navigate("/checkout")}
+        >
+          <FontAwesomeIcon icon={faBagShopping} className="fonticon" />
+          <span>{countProductInBasket}</span>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default BasketInHeader;
