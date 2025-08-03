8303430b1ae39bd2aba6d31b3e26297198b70697
sihs-pashaa <hsynaldswqy54@gmail.com>
2025-08-03 14:50:08 +0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // src/routes/index.jsx
 import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
 
@@ -21,25 +21,6 @@ const router = createBrowserRouter(
 );
 
 export default router;
-=======
-
-// src/routes/index.jsx
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-// Pages
-import BlogPage from "../pages/blog/BlogPage";
-import CheckoutPage from "../pages/checkout/CheckoutPage";
-import HomePage from "../pages/HomePage";
-
-const routes = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-    </>
-  )
-);
-export default routes;
 
 
->>>>>>> 4f678160a1bb3a9098b6f3d1dc6d56ffde08a4df
+
